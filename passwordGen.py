413a4d725735f271768f4d8e3139028d2c7c5774
Krzysztof Maczyński <38271367+ByteEater-pl@users.noreply.github.com>
2022-02-09 23:19:29 +0100

    Refactor extending categories

--- passwordGen.py
+++ passwordGen.py
@@ -2,7 +2,7 @@ def password(n):
 	import secrets, string as S
 	of = secrets.choice
 	cats = [S.ascii_lowercase, S.ascii_uppercase, S.punctuation, S.digits]
-	cats = cats * 2 + [of(cats) for _ in range(n - 8)]
+	cats += cats + [of(cats) for _ in range(n - 8)]
 	for i in range(8):
 		j = of(range(i, n))
 		cats[i], cats[j] = cats[j], cats[i]
